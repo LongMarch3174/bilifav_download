353f4be5af2944bade1b93a9121fc2276bd5a67d
LongMarch3174 <LongMarch3174@icloud.com>
2025-04-22 23:16:20 +0800

    remove special character

--- start_bilifavdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 切换到应用目录
-cd /app || exit 1  # 如果无法进入/app目录，则退出脚本并返回错误代码 1
-
-# 执行 Python 脚本
-python bilifavirousdownload.py >> /proc/1/fd/1 2>> /proc/1/fd/2
--- stup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/etc/init.d/cron start
-
-cd /app || exit 1
-python getCookie_web.py >> /proc/1/fd/1 2>> /proc/1/fd/2
-
-#将容器挂起，防止容器后台启动后自动退出
-tail -f /dev/null
